1eede0992fcf52abfd030c786d2b0e3d7b9b69d8
Abhinov Gogoi <abhinov-gogoi@users.noreply.github.com>
2021-03-24 18:36:12 +0530

    init a repo for this poject

---
+++ src/main/java/com/example/springbootstarter/topic/TopicController.java
@@ -0,0 +1,19 @@
+package com.example.springbootstarter.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @GetMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+}
---
+++ src/main/java/com/example/springbootstarter/topic/TopicService.java
@@ -0,0 +1,23 @@
+package com.example.springbootstarter.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+// In spring, business services are usually singleton classes which are used by other classes
+@Service
+public class TopicService {
+
+    private List<Topic> topics = Arrays.asList(
+            new Topic("1", "Spring framework", "A popular java framework"),
+                new Topic("2", "Maven", "used as build tool"),
+                new Topic("3", "Git", "used as version control tool"),
+                new Topic("4", "Jira", "used as project management tool")
+                );
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+}
